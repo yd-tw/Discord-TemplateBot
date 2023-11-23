e23d29a21166cdd1866b36a9f73b73e0dd90ba5d
yd-tw <yd960528@gmail.com>
2023-11-23 22:13:15 +0800

    update help

--- unit/generally.py
+++ unit/generally.py
@@ -12,4 +12,4 @@ async def version(ctx,VERSION):
     await ctx.send("當前版本:" + VERSION)
 
 async def invite(ctx):
-    await ctx.send("https://discord.com/oauth2/authorize?client_id=1145187780364869651&permissions=0&scope=bot")
+    await ctx.send("邀我到其他伺服器!!!\nhttps://discord.com/oauth2/authorize?client_id=1145187780364869651&permissions=0&scope=bot")
--- unit/helpcode.py
+++ unit/helpcode.py
@@ -2,19 +2,30 @@ import discord
 
 async def help(ctx,START):
     options = [
-        discord.SelectOption(label="基礎指令", emoji="👌", description="指令列表、學說話等基礎雜項指令"),
+        discord.SelectOption(label="基礎指令", emoji="👌", description="邀請連結、學說話、機器人資訊等......"),
         discord.SelectOption(label="語音頻道", emoji="✨", description="加入、離開語音頻道"),
-        discord.SelectOption(label="播放音樂", emoji="🎭", description="選擇音樂、播放、停止等..."),
+        discord.SelectOption(label="播放音樂", emoji="🎭", description="選擇音樂、播放、停止等......"),
         discord.SelectOption(label="設定鬧鐘", emoji="🎭", description="設定鬧鐘來吵你"),
-        discord.SelectOption(label="小小遊戲", emoji="🎭", description="我還在開發，等等我QQ")
+        discord.SelectOption(label="小小遊戲", emoji="🎭", description="我還在開發，等等我 QAQ")
     ]
 
     text = {
-        "基礎指令": f"基礎指令:\n```指令列表 {START}list```\n```學你說話 {START}say```\n```邀請機器人 {START}invite```",
-        "語音頻道": f"語音頻道:\n```加入語音 {START}join```\n```離開語音 {START}leave```",
-        "播放音樂": f"播放音樂:\n```音樂列表 {START}music```\n```播放音樂 {START}play(主檔名)\n停止播放 {START}stop```",
-        "設定鬧鐘": f"設定鬧鐘:\n```設定鬧鐘 {START}alert(Year/Mon/Dat-H:M:S\)```",
-        "小小遊戲": f"小小遊戲:\n就跟你說我還在開發了...不要著急啦"
+        "基礎指令": f"基礎指令:\
+        \n```學你說話 {START}say(要說的話,次數,間隔秒數)```\
+        \n```邀請機器人 {START}invite```\
+        \n```查詢延遲 {START}ping```\
+        \n```機器人版本 {START}version```",
+        "語音頻道": f"語音頻道:\
+        \n```加入語音 {START}join```\
+        \n```離開語音 {START}leave```",
+        "播放音樂": f"播放音樂:\
+        \n```音樂列表 {START}music```\
+        \n```播放音樂 {START}play(主檔名)\
+        \n停止播放 {START}stop```",
+        "設定鬧鐘": f"設定鬧鐘:\
+        \n```設定鬧鐘 {START}alert(Year/Mon/Dat-H:M:S\)```",
+        "小小遊戲": f"小小遊戲:\
+        \n```機率 {START}%(一個問題)"
     }
 
     view = discord.ui.View()
