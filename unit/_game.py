13a99a1e0a3684a4547999ef111cea45ad37ff09
yd-tw <yd960528@gmail.com>
2023-12-03 15:29:29 +0800

    edit instruction and edit readme

--- unit/_game.py
+++ unit/_game.py
@@ -1,6 +1,6 @@
 import random
 
-async def qa(ctx,text):
+async def percent(ctx,text):
     random.seed(text)
     probability = random.uniform(0,100)
     await ctx.respond(f"{text}機率為: {probability:.2f}%")
