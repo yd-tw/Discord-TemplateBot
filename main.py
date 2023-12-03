0940ce98e9c1d00dd70a4c12c856b86bb95142f2
yd-tw <yd960528@gmail.com>
2023-12-03 14:25:42 +0800

    add text

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ bot.help_command = None
 async def on_ready():
     print(f"已成功登入:{bot.user}")
 
-@bot.event
-async def on_message(message):
-    await bot.process_commands(message)
+# @bot.event
+# async def on_message(message):
+#     await bot.process_commands(message)
 
-    if str(message.content)[0]==START:
-        log.info(f"{message.guild},{message.channel},{message.author},{message.content}")
-    else:
-        log.debug(f"{message.guild},{message.channel},{message.author},{message.content}")
+#     if str(message.content)[0]==START:
+#         log.info(f"{message.guild},{message.channel},{message.author},{message.content}")
+#     else:
+#         log.debug(f"{message.guild},{message.channel},{message.author},{message.content}")
 
 @bot.event
 async def on_command_error(ctx,error):
@@ -44,56 +44,56 @@ async def help(ctx, argument=None):
 
 
 #複述對話
-@bot.command(name="say",description="")
+@bot.command(name="say",description="讓機器人幫你說話")
 async def say(ctx, text, times: int = 1, delay: int = 0):
     await _talk.say(ctx, text, times, delay)
 #版本查詢
-@bot.command(name="version",description="")
+@bot.command(name="version",description="顯示機器人版本")
 async def version(ctx):
     await _talk.version(ctx, VERSION)
 #邀請連結
-@bot.command(name="invite",description="")
+@bot.command(name="invite",description="取得機器人邀請連結")
 async def invite(ctx):
     await _talk.invite(ctx)
 #取得延遲
-@bot.command(name="ping",description="")
+@bot.command(name="ping",description="取得機器人延遲")
 async def ping(ctx):
     await _talk.ping(ctx, bot)
 
 
 #加入語音
-@bot.command(name="join",description="")
+@bot.command(name="join",description="加入語音頻道")
 async def join(ctx):
     await _voice.join(ctx)
 #離開語音
-@bot.command(name="leave",description="")
+@bot.command(name="leave",description="離開語音頻道")
 async def leave(ctx):
     await _voice.leave(ctx)
 
 
 #音樂列表
-@bot.command(name="list",description="")
+@bot.command(name="list",description="顯示音樂列表")
 async def list(ctx):
     await _music.list(ctx)
 #播放本地音樂
-@bot.command(name="play",description="")
+@bot.command(name="play",description="播放音樂")
 async def play(ctx, file):
     await _music.play(ctx, file, bot, START, MUSICPATH)
 #停止播放音樂
-@bot.command(name="stop",description="")
+@bot.command(name="stop",description="停止播放音樂")
 async def stop(ctx):
     await _music.stop(ctx, bot)
 
 
 #設定鬧鐘
-@bot.command(name="settime",description="")
+@bot.command(name="settime",description="設定鬧鐘")
 async def settime(ctx, time):
     await _alert.settime(ctx, time)
 
 
 #遊戲
-@bot.command(name="qa",description="")
-async def qa(ctx, text):
+@bot.command(name="percent",description="說一句話，回覆你機率")
+async def qa(ctx, text: discord.Option(str, "輸入希望顯示機率的文字", required=True)):
     await _game.qa(ctx, text)
 
 if __name__ == "__main__":
