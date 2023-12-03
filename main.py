d443a7b95d621936840299bfad12d9fec33f53ad
yd-tw <yd960528@gmail.com>
2023-12-03 13:52:08 +0800

    use / command

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import logging as log
 import discord
-from discord.ext import commands
 from dotenv import load_dotenv
 
 from unit import *
@@ -16,8 +15,8 @@ FORMAT = '%(asctime)s %(levelname)s: %(message)s'
 log.basicConfig(level=log.DEBUG, filename='.log', filemode='a', format=FORMAT, encoding='utf-8')
 
 # intents是要求機器人的權限、command_prefix是前綴符號
-bot = commands.Bot(command_prefix = {START}, intents = discord.Intents.all())
-
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
 bot.help_command = None
 
 #當機器人完成啟動
@@ -45,57 +44,57 @@ async def help(ctx, argument=None):
 
 
 #複述對話
-@bot.command()
+@bot.command(name="say",description="")
 async def say(ctx, text, times: int = 1, delay: int = 0):
     await _talk.say(ctx, text, times, delay)
 #版本查詢
-@bot.command()
+@bot.command(name="version",description="")
 async def version(ctx):
     await _talk.version(ctx, VERSION)
 #邀請連結
-@bot.command()
+@bot.command(name="invite",description="")
 async def invite(ctx):
     await _talk.invite(ctx)
 #取得延遲
-@bot.command()
+@bot.command(name="ping",description="")
 async def ping(ctx):
     await _talk.ping(ctx, bot)
 
 
 #加入語音
-@bot.command()
+@bot.command(name="join",description="")
 async def join(ctx):
     await _voice.join(ctx)
 #離開語音
-@bot.command()
+@bot.command(name="leave",description="")
 async def leave(ctx):
     await _voice.leave(ctx)
 
 
 #音樂列表
-@bot.command()
+@bot.command(name="list",description="")
 async def list(ctx):
     await _music.list(ctx)
 #播放本地音樂
-@bot.command()
+@bot.command(name="play",description="")
 async def play(ctx, file):
     await _music.play(ctx, file, bot, START, MUSICPATH)
 #停止播放音樂
-@bot.command()
+@bot.command(name="stop",description="")
 async def stop(ctx):
     await _music.stop(ctx, bot)
 
 
 #設定鬧鐘
-@bot.command()
+@bot.command(name="settime",description="")
 async def settime(ctx, time):
     await _alert.settime(ctx, time)
 
 
 #遊戲
-@bot.command()
-async def QA(ctx, text):
-    await _game.QA(ctx, text)
+@bot.command(name="qa",description="")
+async def qa(ctx, text):
+    await _game.qa(ctx, text)
 
 if __name__ == "__main__":
     bot.run(TOKEN)
--- unit/_game.py
+++ unit/_game.py
@@ -1,6 +1,6 @@
 import random
 
-async def QA(ctx,text):
+async def qa(ctx,text):
     random.seed(text)
     probability = random.uniform(0,100)
     await ctx.send(f"{text}機率為: {probability:.2f}%")
